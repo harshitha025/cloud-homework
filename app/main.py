65908e37bca9165961403dd6a324d94de49e2000
harshitha025 <harshvemula01@gmail.com>
2026-03-28 00:46:37 -0500

    added s3 integration

--- app/main.py
+++ app/main.py
@@ -9,3 +9,18 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+import boto3
+
+s3 = boto3.client('s3')
+
+@app.get("/upload")
+def upload():
+    file_name = "test.txt"
+    
+    with open(file_name, "w") as f:
+        f.write("Hello from EC2")
+
+    s3.upload_file(file_name, "cloud-homework-harsh-123", file_name)
+
+    return {"status": "uploaded to s3"}
